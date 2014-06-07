c013ccc3bb2dc4b3e825253566683ab7d62a86d2
Reese Hodge <quattro004@gmail.com>
2014-06-07 08:28:42 -0700

    Created a UCI project with a failing test for intializing the engine.

---
+++ ChessterUCI/UniversalChessInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessterUCI
+{
+    public class UniversalChessInterface
+    {
+        public bool EngineInitialized { get; set; }
+
+        public void InitializeEngine()
+        {
+        }
+    }
+}
---
+++ TestChessterUCI/TestUniversalChessInterface.cs
@@ -0,0 +1,21 @@
+﻿using ChessterUCI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestChessterUCI
+{
+    public class TestUniversalChessInterface
+    {
+        [Fact]
+        public void initialize_engine()
+        {
+            var uciClient = new UniversalChessInterface();
+            uciClient.InitializeEngine();
+            Assert.True(uciClient.EngineInitialized, "The chess engine was not initialized properly.");
+        }
+    }
+}
