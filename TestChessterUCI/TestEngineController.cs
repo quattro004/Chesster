ab23e0d4e34aad7557a26a7d171e4a74f97a9f00
Reese Hodge <quattro004@gmail.com>
2014-06-08 09:15:34 -0700

    Created a TestEngineController class for testing the chess engine controller. Moved the start_engine_process test to it.

---
+++ TestChessterUCI/TestEngineController.cs
@@ -0,0 +1,27 @@
+﻿using ChessterUCI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestChessterUCI
+{
+    public class TestEngineController
+    {
+        [Fact]
+        public void start_engine_process()
+        {
+            var engineController = new EngineController();
+            using (var engineProcess = engineController.StartChessEngine())
+            {
+                Assert.NotNull(engineProcess);
+                Assert.True(engineProcess.StartInfo.RedirectStandardInput, "Standard input has not been redirected.");
+                Assert.True(engineProcess.StartInfo.RedirectStandardError, "Standard error has not been redirected.");
+                Assert.True(engineProcess.StartInfo.RedirectStandardOutput, "Standard output has not been redirected.");
+                engineProcess.Close();
+            }
+        }
+    }
+}
