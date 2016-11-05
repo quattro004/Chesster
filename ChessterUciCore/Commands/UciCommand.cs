43782c342dd30128159d05a33d2faae7257d2bf0
Reese <quattro004@gmail.com>
2016-11-05 08:44:10 -0700

    Added support for copy protection if the engine sends that message.

--- ChessterUciCore/Commands/UciCommand.cs
+++ ChessterUciCore/Commands/UciCommand.cs
@@ -64,6 +64,15 @@ namespace ChessterUciCore.Commands
         public Dictionary<string, OptionData> Options { get; } = new Dictionary<string, OptionData>();
 
         /// <summary>
+        /// This is needed for copyprotected engines. After the uciok command the engine can tell the GUI, 
+        /// that it will check the copy protection now. This is done by "copyprotection checking". If the check 
+        /// is ok the engine should send "copyprotection ok", otherwise "copyprotection error". If there is an
+        /// error the engine should not function properly but should not quit alone. If the engine reports 
+        /// "copyprotection error" the GUI should not use this engine and display an error message instead!
+        /// </summary>
+        public string CopyProtection { get; private set; }
+
+        /// <summary>
         /// Performs disposal for this command.
         /// </summary>
         /// <param name="disposing"></param>
@@ -102,6 +111,10 @@ namespace ChessterUciCore.Commands
                 else if (data.StartsWith(UCIOK))
                 {
                     CommandResponseReceived = true;
+                }
+                else if (data.StartsWith("copyprotection"))
+                {
+                    CopyProtection = data;
                 }
             }
         }
